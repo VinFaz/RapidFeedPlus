956a62267a8d7146c4fcf3d710ee601d46261f0d
VinFaz <72625761+VinFaz@users.noreply.github.com>
2020-10-09 15:51:18 -0400

    Cleanup
    removed uneeded imports and spaced the functions by 2 lines.

--- Main.py
+++ Main.py
@@ -1,14 +1,8 @@
-import time
-import random
-import math
 import tkinter
-from tkinter import Label, Button, Entry, PhotoImage
 from tkinter import filedialog
 from tkinter import *
 from tkinter.font import Font
 global RapidSpeed
-global updateWindow
-updateWindow = True
 
 #get file and add F(num) to all G0s
 def RapidFeedMod():
@@ -30,6 +24,8 @@ def RapidFeedMod():
                 print("G0 at line " + str(i + 1))
                 LineRead[i] = LineRead[i].strip() + " F" + str(RapidFeedSpeed) + "\n"
     newFile()
+
+
 def newFile():
     newfile = open(filename + " (Rapid)", "w+")
     global newfilename
@@ -39,11 +35,13 @@ def newFile():
     file.close()
     print("File Closed!")
 
+
 def setFileName():
     global filename
     filename = filedialog.askopenfilename()
     print(filename)
     implabel.config(text=filename)
+
 
 #Tkinter window
 app = tkinter.Tk()
@@ -51,7 +49,7 @@ text = tkinter.Text(app)
 mainfont = Font(family="Exo", size=20, weight="bold")
 app.title("Rapid Feed-ifier")
 app.configure(bg="#363636")
-app.iconphoto(False, PhotoImage(file="D:\Pictures\Py Projects\Rapid Speed Restoration\Drill_bit-512.png"))
+app.iconphoto(False, PhotoImage(file="Drill_bit-512.png"))
 app.resizable(width=False, height=False)
 RapidSpeed = Entry(app, bg="#8f8f8f", fg="#d9d9d9", font=mainfont)
 impfile = Button(app, text="Import File", command=setFileName, bg="#616161", fg="#d9d9d9", font=mainfont)
@@ -59,6 +57,7 @@ SpeedLabel = Label(text="Rapid Speed (mm/min)", bg="#363636", fg="#d9d9d9", font
 implabel = Label(text=setFileName, fg="#d9d9d9", bg="#363636")
 expfile = Button(app, text="Export File", command=RapidFeedMod, bg="#414141", fg="#d9d9d9", font=mainfont)
 explabel = Label(text=newFile, fg="#d9d9d9", bg="#363636")
+
 
 #Ordering of Buttons/Interactive modules
 SpeedLabel.pack()
